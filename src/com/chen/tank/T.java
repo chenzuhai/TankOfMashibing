7c0bf97627203559a0bba2ff689605f9f0324a06
chenzuhai <1808966946@qq.com>
2021-01-07 15:23:20 +0800

    change

--- src/com/chen/tank/T.java
+++ src/com/chen/tank/T.java
@@ -10,6 +10,13 @@ public class T {
 
     public static void main(String[] args) {
         TankFrame tf = new TankFrame();
-
+//        while (true) {
+//            try {
+//                Thread.sleep(50);
+//                tf.repaint();
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//        }
     }
 }
--- src/com/chen/tank/TankFrame.java
+++ src/com/chen/tank/TankFrame.java
@@ -1,31 +1,102 @@
 package com.chen.tank;
 
 import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
 /**
+ * 从Frame类继承
+ *
  * @author Zuhai Chen
  * @version 1.0
  * @date 2021/1/7 13:18
  */
 public class TankFrame extends Frame {
-    public TankFrame(){
-        setSize(800,600);
+
+    int x = 200, y = 200;
+
+    public TankFrame() {
+        setSize(800, 600);
         setResizable(false);
         setTitle("tank war");
         setVisible(true);
         addWindowListener(new WindowAdapter() {
             @Override
             public void windowClosing(WindowEvent e) {
-                System.out.println("666");
                 System.exit(0);
             }
         });
+        this.addKeyListener(new MyKeyListener());
     }
 
+    /**
+     * 这个方法就是重新画
+     *
+     * @param g
+     */
     @Override
     public void paint(Graphics g) {
-        g.fillRect(200,200,50,50);
+        //System.out.println("paint");
+        g.fillRect(x, y ,50, 50);
+        //x += 1;
+        //y += 1;
+    }
+
+    /**
+     * 键盘监听处理的类
+     */
+    class MyKeyListener extends KeyAdapter{
+
+        boolean bL = false;
+        boolean bU = false;
+        boolean bR = false;
+        boolean bD = false;
+
+        @Override
+        public void keyPressed(KeyEvent e) {
+            int keyCode = e.getKeyCode();
+            switch (keyCode){
+                case KeyEvent.VK_LEFT:
+                    bL = true;
+                    break;
+                case KeyEvent.VK_RIGHT:
+                    bR = true;
+                    break;
+                case KeyEvent.VK_UP:
+                    bU = true;
+                    break;
+                case KeyEvent.VK_DOWN:
+                    bD = true;
+                    break;
+                default:
+                    break;
+            }
+            //x+=30;
+            //repaint()会调用paint(Graphics g)
+            repaint();
+        }
+
+        @Override
+        public void keyReleased(KeyEvent e) {
+            int keyCode = e.getKeyCode();
+            switch (keyCode){
+                case KeyEvent.VK_LEFT:
+                    bL = false;
+                    break;
+                case KeyEvent.VK_RIGHT:
+                    bR = false;
+                    break;
+                case KeyEvent.VK_UP:
+                    bU = false;
+                    break;
+                case KeyEvent.VK_DOWN:
+                    bD = false;
+                    break;
+                default:
+                    break;
+            }
+        }
     }
 }
